a17b0e4a65836086af681412dd9581d87ab4885c
Oluwatoyin <toyinoyelade21@gmail.com>
2024-10-21 00:19:02 +0100

    4th commit

--- src/pages/user/[username].tsx
+++ src/pages/user/[username].tsx
@@ -1,3 +1,47 @@
+// import { useRouter } from "next/router";
+// import { useEffect, useState } from "react";
+// import { fetchUserProfile, fetchUserRepos } from "@/lib/api";
+// import UserProfile from "@/Components/UserProfile";
+// import RepositoryList from "@/Components/RepositoryList";
+
+// const UserPage = () => {
+//   const router = useRouter();
+//   const { username } = router.query;
+//   const [user, setUser] = useState<any>(null);
+//   const [repos, setRepos] = useState<any[]>([]);
+//   const [page, setPage] = useState(1);
+
+//   useEffect(() => {
+//     if (username) {
+//       fetchUserProfile(username as string)
+//         .then(setUser)
+//         .catch(console.error);
+//       fetchUserRepos(username as string)
+//         .then(setRepos)
+//         .catch(console.error);
+//     }
+//   }, [username]);
+
+//   const loadMoreRepos = () => {
+//     fetchUserRepos(username as string, page + 1)
+//       .then((newRepos) => {
+//         setRepos([...repos, ...newRepos]);
+//         setPage(page + 1);
+//       })
+//       .catch(console.error);
+//   };
+
+//   return user ? (
+//     <div className="container mx-auto p-4">
+//       <UserProfile username={user} />
+//       <RepositoryList repos={repos} onNextPage={loadMoreRepos} />
+//     </div>
+//   ) : (
+//     <p>Loading...</p>
+//   );
+// };
+
+// export default UserPage;
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { fetchUserProfile, fetchUserRepos } from "@/lib/api";
@@ -33,7 +77,7 @@ const UserPage = () => {
 
   return user ? (
     <div className="container mx-auto p-4">
-      <UserProfile username={user} />
+      <UserProfile user={user} /> {/* Pass user, not username */}
       <RepositoryList repos={repos} onNextPage={loadMoreRepos} />
     </div>
   ) : (
